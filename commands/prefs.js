e449589ba082dc9afa542e8a051d1d4eba372524
Alex Taxiera <alex.taxiera@gmail.com>
2017-06-22 01:03:24 -0400

    update preferences embed

--- commands/prefs.js
+++ commands/prefs.js
@@ -1,4 +1,5 @@
 const func = require('../common.js')
+const moment = require('moment')
 module.exports = {
   command: 'prefs',
   description: 'Display current bot preferences',
@@ -9,22 +10,32 @@ module.exports = {
     var guild = global.bot.Guilds.toArray().find(g => g.id === client.guild.id)
     var vipRole = getCleanVipRole(client, guild)
     var gameRoles = getCleanGameRoles(client, guild)
-
-    var str = 'Preferences'
+    var member = guild.members.find(m => m.id === global.bot.User.id)
     var embed =
       {
+        description: '```          PREFERENCES```',
+        'timestamp': moment(),
         color: 0x3498db,
-        fields: [{name: 'Default Text Channel', value: client.textChannel.name},
-            {name: 'Default Voice Channel', value: client.voiceChannel.name},
-            {name: 'VIP Role', value: vipRole},
-            {name: 'Autoplay', value: client.autoplay},
-            {name: 'Announce Now Playing', value: client.informNowPlaying},
-            {name: 'Announce Now Playing from Autoplay', value: client.informAutoPlaying},
-            {name: 'Memes', value: client.meme},
-            {name: 'Music Volume', value: `${client.volume * 2}`},
-            {name: 'Game Roles', value: gameRoles}]
+        'footer': {
+          'icon_url': 'https://raw.githubusercontent.com/alex-taxiera/TuxedoMan/indev/images/tuxedoman.png',
+          'text': 'TuxedoMan'
+        },
+        'author': {
+          'name': member.nick,
+          'url': 'https://github.com/alex-taxiera/TuxedoMan',
+          'icon_url': member.avatarURL
+        },
+        fields: [{name: 'Default Text Channel', value: client.textChannel.name, inline: true},
+            {name: 'Default Voice Channel', value: client.voiceChannel.name, inline: true},
+            {name: 'VIP Role', value: vipRole, inline: true},
+            {name: 'Autoplay', value: client.autoplay, inline: true},
+            {name: 'Announce Now Playing', value: client.informNowPlaying, inline: true},
+            {name: 'Announce Autoplay', value: client.informAutoPlaying, inline: true},
+            {name: 'Memes', value: client.meme, inline: true},
+            {name: 'Music Volume', value: `${client.volume * 2}`, inline: true},
+            {name: 'Game Roles', value: gameRoles, inline: true}]
       }
-    return {promise: msg.reply(str, false, embed), content: str, delay: 25000, embed: embed}
+    return {promise: msg.channel.sendMessage('', false, embed), content: '', delay: 25000, embed: embed}
   }
 }
 function getCleanVipRole (client, guild) {
