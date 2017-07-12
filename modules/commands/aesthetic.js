56a797c68db732bd74d3024952f005301991f5bc
Alex Taxiera <alex.taxiera@gmail.com>
2017-07-12 15:03:07 -0400

    fix aesthetic spacing

--- modules/commands/aesthetic.js
+++ modules/commands/aesthetic.js
@@ -1,22 +1,18 @@
 module.exports = {
   command: 'aesthetic',
-  description: 'make your text *a e s t h e t i c*',
+  description: 'make your text ａｅｓｔｈｅｔｉｃ',
   parameters: [],
   rank: 0,
   execute: function (msg, params) {
-    let fullParam = params.join(' ')
     let str = ''
+
+    let fullParam = params.join(' ')
     for (let i = 0; i < fullParam.length; i++) {
-      let result = ''
       let charCode = fullParam[i].charCodeAt()
       if (charCode < 127 && charCode !== 32) {
-        result += String.fromCharCode((charCode + 65248))
+        str += String.fromCharCode((charCode + 65248))
       } else {
-        result += fullParam[i]
-      }
-      str += result
-      if (i !== params.length - 1) {
-        str += ' '
+        str += fullParam[i]
       }
     }
     msg.channel.sendMessage(str)
