ab9de205c12afe9ef1bc90843a23c5bcfe786ccb
Alex Taxiera <alex.taxiera@gmail.com>
2018-02-09 11:11:01 -0500

    remove stale memes

--- modules/memes.js
+++ modules/memes.js
@@ -36,18 +36,6 @@ module.exports = function (msg, text) {
   if (text.includes(' dva ') || text === 'dva') {
     sendFile(msg.channel, 'kek.png')
   }
-  // ayya
-  if (text.includes(' ayya ') || text === 'ayya') {
-    msg.channel.createMessage('AYYA AYYA AYYA')
-  }
-  // panda
-  if (text.includes(' panda ') || text === 'panda') {
-    msg.channel.createMessage('Panda\nPanda\nPanda\nPanda\nPanda')
-  }
-  // stain
-  if (text.includes(' stain ') || text === 'stain') {
-    msg.channel.createMessage('STAIN STAIN STAIN STAIN STAIN STAIN STAIN STAIN STAIN STAIN STAIN')
-  }
   // baby
   if (text.includes(' baby ') || text === 'baby') {
     sendFile(msg.channel, 'baby.gif')
@@ -55,24 +43,6 @@ module.exports = function (msg, text) {
   // ban
   if (text.includes(' ban ') || text === 'ban') {
     sendFile(msg.channel, 'ban.jpg')
-  }
-  // bb
-  if (text.includes(' bb ') || text === 'bb') {
-    msg.channel.createMessage('Big Brother is watching™')
-  }
-  // blueberry
-  if (text.includes(' blueberry pie ') || text === 'blueberry pie') {
-    msg.channel.createMessage('BLUEBERRY FUCKING PIE? WHAT KIND OF FILTHY, ' +
-    'UNWASHED, DEGENERATES DECIDED TO COME UP WITH THIS SHIT. FIRST YOU GIVE ' +
-    'PEOPLE THE POWER TO DICTATE THEIR CREAM FILLING, NOW YOU\'RE LETTING THEM ' +
-    'CONDENSE A HOME COOKED PASTRY INTO A BITE SIZED CRUMPET SHIT? REALLY? FUCKING ' +
-    'REALLY? I AM GOING TO FIND WHEVER MADE THIS ONLY TO DISEMBOWLE THEM, INFLATE ' +
-    'THEIR ORGANS AND SHOVE THEM BACK INSIDE, SO THAT THEIR BODY RESEMBLES THE ' +
-    'BLUEBERRY GIRL IN CHARLIE AND THE CHOCOLA-FUCKING-TE FACTA-FUCKING-ORY. THAT\'S ' +
-    'RIGHT. THAT\'S WHO CREATED THIS. IT\'S EVIL AND I SHALL HAVE NO PART OF IT. IF ' +
-    'YOU HAVE A CONCIENSE, OR ANY SEMPLENCE OF A SOUL, YOU WILL THROW THOSE AWAY, OR ' +
-    'BETTER YET, BURN THEM AND SPREAD THEIR ASHES THOROUGHLY INTO A VENUS FLYTRAP ' +
-    'FLOWERBED. THAT IS ALL.')
   }
   // boob
   if (text.includes(' boob ') || text === 'boob') {
@@ -90,10 +60,6 @@ module.exports = function (msg, text) {
   if (text.includes(' daddy ') || text === 'daddy') {
     msg.channel.createMessage('<@192158164798406658>')
   }
-  // danganroppa
-  if (text.includes('danganroppa')) {
-    msg.channel.createMessage('Dangit Wrongpan?')
-  }
   // debbie
   if (text.includes('debbie')) {
     msg.channel.createMessage('WHAT WILL DEBBIE THINK!')
@@ -105,12 +71,6 @@ module.exports = function (msg, text) {
   // doyoueven
   if (text.includes(' doyoueven') || text === 'doyoueven' || text.includes('do you even ') || text === 'do you even') {
     sendFile(msg.channel, 'doyoueven.jpg')
-  }
-  // dozicus
-  if (text.includes('dozicus')) {
-    msg.channel.createMessage('DozicusPrimeTheDestroyerOfWorldsFredButtonIdiot' +
-    'MushroomBurger Stormborn of house targaryen, first of her name, queen of ' +
-    'the andals and first men, khaleesi, mother of dragons and breaker of chains.')
   }
   // embargo
   if (text.includes('embargo')) {
@@ -151,10 +111,6 @@ module.exports = function (msg, text) {
   if (text.includes('iwata')) {
     sendFile(msg.channel, 'iwata.jpg')
   }
-  // jon
-  if (text.includes(' jon ') || text === 'jon') {
-    sendFile(msg.channel, 'jon.gif')
-  }
   // kevin
   if (text.includes('birthday')) {
     msg.channel.createMessage('HAPPY BIRTHDAY <@119963118016266241>')
@@ -162,33 +118,6 @@ module.exports = function (msg, text) {
   // left
   if (text.includes(' left ') || text === 'left') {
     sendFile(msg.channel, 'left.jpg')
-  }
-  // lmao
-  if (text.includes('lmao')) {
-    client.lmaoCount++
-    if (client.lmaoCount % 20 === 0) {
-      msg.channel.createMessage('What the ayy did you just fucking lmao about ' +
-      'me, you ayy lmao? I\'ll have you know I graduated top of my ayy in ' +
-      'the Lmaos, and I\'ve been involved in numerous Lmao\'s on Ayyl-Quaeda' +
-      ', and I have over 300 confirmed lmaos. I am trained in ayy lmao and ' +
-      'I\'m the top ayy in the entire US lmao. You are nothing to me but just ' +
-      'another ayy. I will ayy you the fuck lmao with ayy the likes of which ' +
-      'has never been seen lmao\'d on this Earth, mark my ayy lmao. You think ' +
-      'you can get away with ayying that lmao to me over the Internet? Think ' +
-      'again, fucker. As we voiceSpeak I am ayying my secret network of lmaos ' +
-      'across the USA and your ayy is being traced right now so you better ' +
-      'prepare for the lmao, maggot. The lmao that ayys out the pathetic little ' +
-      'thing you call your lmao. You\'re ayy lmao, kid. I can ayy anywhere, ' +
-      'anytime, and I can lmao you in over seven hundred ways, and that\'s ' +
-      'just with my bare lmao. Not only am I extensively trained in ayy lmao' +
-      ', but I have access to the entire ayy of the United States Lmao and I ' +
-      'will use it to its full extent to ayy your miserable lmao off the face ' +
-      'of the continent, you little shit. If only you could have known what ' +
-      'unholy ayy your little “clever” lmao was about to bring down upon you' +
-      ', maybe you would have held your fucking ayy. But you couldn’t, you ' +
-      'didn’t, and now you’re ayying the lmao, you goddamn idiot. I will ayy ' +
-      'lmao all over you and you will ayy in it. You’re fucking lmao, kiddo')
-    }
   }
   // mao
   if (text.includes(' mao ') || text === 'mao') {
@@ -232,14 +161,6 @@ module.exports = function (msg, text) {
   // petyr
   if (text.includes('petyr')) {
     sendFile(msg.channel, 'petyr.jpeg')
-  }
-  // pls
-  if (text.includes('please the team') || text.includes('pleasetheteam') || text === 'pls') {
-    sendFile(msg.channel, 'pls.gif', 30000)
-  }
-  // poopkink
-  if (text.includes('poopkink')) {
-    msg.channel.createMessage('http://www.poopkink.com')
   }
   // pushthepayload
   if (text.includes('payload')) {
