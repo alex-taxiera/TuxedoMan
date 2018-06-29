c57698f388f4979ee8496d78c51abcd5df810db5
Alex Taxiera <alex.taxiera@gmail.com>
2018-06-29 15:28:09 -0400

    document

--- src/TuxedoMan/TuxedoMan.js
+++ src/TuxedoMan/TuxedoMan.js
@@ -21,6 +21,11 @@ class TuxedoMan extends require('../classes/DataClient.js') {
    * @param {String}  config.DEFAULT.status.name     The default bot status name.
    * @param {Number}  config.DEFAULT.status.type     The default bot status type.
    * @param {Boolean} config.DEFAULT.status.default  The boolean indicating to the database that this is the default status.
+   * @param {Object}  config.ROLES                   The extra role names to use.
+   * @param {String}  config.ROLES.gameRole          The Other Games role name.
+   * @param {String}  config.ROLES.listenRole        The Listening role name.
+   * @param {String}  config.ROLES.watchRole         The Watching role name.
+   * @param {String}  config.ROLES.streamRole        The Streaming role name.
    * @param {Object}  options                        Same as Client.
    */
   constructor (config, options) {
