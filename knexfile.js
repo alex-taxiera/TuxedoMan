00eb8a6b1db785b321b96018308163e4e7e6dd1c
Alex Taxiera <alex.taxiera@onesky.com>
2020-03-20 22:44:42 -0400

    correct knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,32 +1,49 @@
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV === 'production') {
+  require('docker-secret-env').load()
+} else {
   require('dotenv').config()
 }
 
 const {
-  NODE_ENV,
-  TUX_DB_CLIENT,
-  TUX_DB_NAME,
-  TUX_DB_USER,
-  TUX_DB_PASS,
-  TUX_DB_HOST,
-  TUX_DB_CONNECTION
+  DB_CLIENT,
+  DB_NAME,
+  DB_USER,
+  DB_PASS,
+  DB_HOST,
+  ADMIN_DB_USER,
+  ADMIN_DB_PASS
 } = process.env
 
+const baseConfig = {
+  client: DB_CLIENT,
+  connection: {
+    host: DB_HOST,
+    database: DB_NAME
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+}
+
 module.exports = {
-  [NODE_ENV]: {
-    client: TUX_DB_CLIENT,
-    connection: TUX_DB_CONNECTION || {
-      host: TUX_DB_HOST,
-      database: TUX_DB_NAME,
-      user: TUX_DB_USER,
-      password: TUX_DB_PASS
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+  production: {
+    ...baseConfig,
+    connection: {
+      ...baseConfig.connection,
+      user: ADMIN_DB_USER,
+      password: ADMIN_DB_PASS
+    }
+  },
+  development: {
+    ...baseConfig,
+    connection: {
+      ...baseConfig.connection,
+      user: DB_USER,
+      password: DB_PASS
     }
   }
 }
